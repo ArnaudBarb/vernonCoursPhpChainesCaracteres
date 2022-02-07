5e5b5f2fe90646870708bec44d775d7f4a17b22c
Arnaud Barberon <barberon_a@hotmail.fr>
2022-02-07 14:33:32 +0100

    ex avec l'équivalent php

--- index3.php
+++ index3.php
@@ -9,3 +9,24 @@ if(preg_match('/chat/', $texte)){
 else {
     echo "La phrase ne contient pas le mot 'chat'";
 }
+echo "<hr />";
+
+$texte2 = "J'ai adopté un chat";
+
+if(preg_match('/un chat|un chien/', $texte2)){
+    echo "La phrase contient l'expression un 'chat' ou un 'chien'";
+}
+else {
+    echo "La phrase ne contient ni le mot 'chat' ni le mot 'chien'";
+}
+echo "<hr />";
+
+if(strpos($texte2, 'un chat') !== false || strpos($texte2, 'un chien')) {
+    echo "chien ou chat";
+}
+else {
+    echo "Ni chien ni chat";
+}
+echo "<hr />";
+
+
